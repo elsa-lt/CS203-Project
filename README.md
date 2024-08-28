7b93476d49a8ab710fa9078aeafed259cf702f95
Elsa Lee <els4lee@gmail.com>
2024-08-28 12:33:11 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CS203-Project
+ CS203 Tournament Management System
---
+++ src/test/java/com/cs203proj/Cs203projApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs203proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs203projApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

8c841f1b2c7e71bbe20a4f16c90154c26aa83956
Elsa Lee <els4lee@gmail.com>
2024-08-28 16:53:50 +0800

    Initial frontend React setup

---
+++ frontend/tetra-league-ui/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+
+    return (
+        <div>
+            <h1 className="text-center">Tetra League</h1>
+        </div>
+    );
+}
+
+export default App;

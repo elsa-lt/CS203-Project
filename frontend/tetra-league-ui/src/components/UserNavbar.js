0a058a1aad85b7e42366f439a2ff0a97f6cddb75
geraldloi <gerald.loi.2023@scis.smu.edu.sg>
2024-09-24 15:39:48 +0800

    Edited UserNavbar
    - added notification button
    - added profile pic & drop down

--- frontend/tetra-league-ui/src/components/UserNavbar.js
+++ frontend/tetra-league-ui/src/components/UserNavbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { FaSignOutAlt } from 'react-icons/fa'; 
+import { IoNotificationsOutline } from "react-icons/io5";
+import ProfileDropDown from '../components/ProfileDropDown';
 
 const UserNavbar = () => {
   const location = useLocation();
@@ -8,7 +9,7 @@ const UserNavbar = () => {
   return (
     <nav className="bg-white p-1 fixed top-0 left-0 w-full z-50 flex justify-between items-center">
       <div className="flex flex-shrink-0">
-        <div className="text-Gray-Text-Colour text-3xl font-bold mr-8">
+        <div className="text-customGray text-3xl font-bold mr-8">
           <Link to="/home">
           <img src="/Headers/Tetra League Logo.png" alt="Logo" className="h-10" />
           </Link>
@@ -17,19 +18,18 @@ const UserNavbar = () => {
       {/*RHS Element Container*/}
       <div className = "flex">
         {/*Header Container*/}
-        <ul className="text-Gray-Text-Colour flex text-l space-x-6">
+        <ul className="flex text-l text-center text-customGray space-x-6">
           <li>
             <Link
               to="/current-match"
-              className={`font-light Helvetica Neue Gray-Text-Colour ${location.pathname === '/current-match' ? 'font-medium text-yellow-400' : 'hover:text-yellow-400'}`}
-            >
+              className={`font-light helvetica-neue customGray ${location.pathname === '/current-match' ? 'font-medium text-yellow-400' : 'hover:text-yellow-400'}`} >
               Current Match
             </Link>
           </li>
           <li>
             <Link
               to="/tournaments"
-              className={`font-light Helvetica Neue Gray-Text-Colour ${location.pathname === '/tournaments' ? 'font-medium text-yellow-400' : 'hover:text-yellow-400'}`}
+              className={`font-light helvetica-eue customGray ${location.pathname === '/tournaments' ? 'font-medium text-yellow-400' : 'hover:text-yellow-400'}`}
             >
               Tournaments
             </Link>
@@ -37,7 +37,7 @@ const UserNavbar = () => {
           <li>
             <Link
               to="/leaderboard"
-              className={`font-light Helvetica Neue Gray-Text-Colour ${location.pathname === '/leaderboard' ? 'font-medium text-yellow-400' : 'hover:text-yellow-400'}`}
+              className={`font-light helvetica-neue customGray ${location.pathname === '/leaderboard' ? 'font-medium text-yellow-400' : 'hover:text-yellow-400'}`}
             >
               Leaderboard
             </Link>
@@ -45,7 +45,7 @@ const UserNavbar = () => {
           <li>
             <Link
               to="/match-history"
-              className={`font-light Helvetica Neue Gray-Text-Colour ${location.pathname === '/match-history' ? 'font-medium text-yellow-400' : 'hover:text-yellow-400'}`}
+              className={`font-light helvetica-neue customGray ${location.pathname === '/match-history' ? 'font-medium text-yellow-400' : 'hover:text-yellow-400'}`}
             >
               Match History
             </Link>
@@ -53,18 +53,20 @@ const UserNavbar = () => {
           <li>
             <Link
               to="/profile"
-              className={`font-light Helvetica Neue Gray-Text-Colour ${location.pathname === '/profile' ? 'font-medium text-yellow-400' : 'hover:text-yellow-400'}`}
+              className={`font-light helvetica-neue customGray ${location.pathname === '/profile' ? 'font-medium text-yellow-400' : 'hover:text-yellow-400'}`}
             >
               Profile
             </Link>
           </li>
         </ul>
-      {/*sign out button*/}
-        <div className="flex ms-6 me-6">
-          <button className="text-Gray-Text-Colour hover:text-yellow-400">
-            <FaSignOutAlt className="text-2xl" />
+
+        <div className="flex ml-6 mr-6">
+          <button className="text-Custom-Gray hover:text-yellow-400 cursor-pointer">
+            <IoNotificationsOutline className="text-2xl" />
           </button>
         </div>
+
+        <ProfileDropDown />
       </div>
     </nav>
   );

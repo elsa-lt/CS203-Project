6ee8bb6f542a861668863e915b39626b08416a18
Varidhi <161668398+Varidhi123456@users.noreply.github.com>
2024-09-25 01:00:31 +0800

    fix imported component

--- frontend/tetra-league-ui/src/pages/admin/CreateTournamentPage.js
+++ frontend/tetra-league-ui/src/pages/admin/CreateTournamentPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../../components/AdminNavbar';
+import Navbar from '../../components/AdminSidebar';
 import { useState } from 'react';
 
 const CreateTournamentsPage = () => {

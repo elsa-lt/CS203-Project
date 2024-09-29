27c4a0005a0f7f5c98c2abe915f3ad46d95e5374
Varidhi <161668398+Varidhi123456@users.noreply.github.com>
2024-09-29 17:00:21 +0800

    past tournaments header added

--- frontend/tetra-league-ui/src/pages/admin/AdminPastTournamentsPage.js
+++ frontend/tetra-league-ui/src/pages/admin/AdminPastTournamentsPage.js
@@ -11,6 +11,11 @@ const AdminPastTournamentsPage = () => {
       className="flex-1 min-h-screen bg-cover bg-center p-6 ml-80" 
       style={{ backgroundImage: `url('/Background/White Background.png')` }}
     >
+      <div className="w-1/2 p-8 relative">
+          <div className="relative top-10 left-60 text-center">
+            <img src="/Headers/Past Tournaments Header.png" alt="Profile" className="w-72 md:w-6/12 -ml-80" style={{ width: '550px' }} />
+          </div>
+          </div>
       
     </main>
   </div>

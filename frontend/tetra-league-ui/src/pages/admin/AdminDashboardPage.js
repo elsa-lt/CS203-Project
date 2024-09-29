d3daf4c4b9f5f22d6ed12370020632a3d8f16c0a
Varidhi <161668398+Varidhi123456@users.noreply.github.com>
2024-09-29 13:06:20 +0800

    admin dashboard header

--- frontend/tetra-league-ui/src/pages/admin/AdminDashboardPage.js
+++ frontend/tetra-league-ui/src/pages/admin/AdminDashboardPage.js
@@ -16,12 +16,22 @@ const AdminDashboardPage = () => {
         <Navbar className="fixed z-40"/>
       </div>
 
-      <div className="flex">
-        <div className="text-4xl font-bold font-press-start mb-4 ml-60 text-gray-700">
-          DASHBOARD
+      <div className="flex flex-col min-h-screen w-full mt-14 ml-60 mr-0">
+      
+        <div className = "flex flex-grow-0 w-30 h-10">
+          <img 
+            src="/Headers/Admin dashboard.png"
+            alt="Dashboard Header"
+            className="w-100 h-30">
+          </img>
         </div>
         <div className="flex flex-col space-y-6">
           <div className="flex flex-wrap gap-6">
+          <img 
+            src="/Online Images/nintendo-switch.png"
+            className="w-20 h-20 mt-7"
+            style={{ width: '120px', height: '120px'}}>
+          </img>
             <Cards />
           </div>
           <div className="mt-6">

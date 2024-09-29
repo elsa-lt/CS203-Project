df1aba436035ba29f3e461c7574e2023a81ee783
Varidhi <161668398+Varidhi123456@users.noreply.github.com>
2024-09-29 13:10:43 +0800

    admin announcement modal

--- frontend/tetra-league-ui/src/components/AnnouncementModal.js
+++ frontend/tetra-league-ui/src/components/AnnouncementModal.js
@@ -27,13 +27,13 @@ const AnnouncementModal = ({ isOpen, onClose, onAddAnnouncement }) => {
 
   return (
     isOpen ? (
-      <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-        <div className="bg-gray-800 p-6 rounded-lg w-11/12 md:w-1/3 relative">
+      <div className="fixed inset-0 bg-white bg-opacity-50 flex items-center justify-center z-50">
+        <div className="bg-white p-6 rounded-lg w-11/12 md:w-1/3 relative">
           <div className="flex justify-between items-center mb-4">
-            <h2 className="text-2xl font-bold font-press-start text-gray-100">Add New Announcement</h2>
+            <h2 className="text-2xl font-bold font-press-start text-black mb-7 mt-7">New Announcement</h2>
             <button
               onClick={onClose}
-              className="text-gray-100 hover:text-gray-200"
+              className="text-black hover:text-gray-200"
               aria-label="Close"
             >
               <FaTimes className="text-2xl" />
@@ -41,7 +41,7 @@ const AnnouncementModal = ({ isOpen, onClose, onAddAnnouncement }) => {
           </div>
           <form onSubmit={handleSubmit}>
             <div className="mb-4">
-              <label className="block text-gray-200 text-sm font-bold mb-2" htmlFor="title">
+              <label className="block text-black text-sm font-bold mb-2" htmlFor="title">
                 Title
               </label>
               <input
@@ -49,28 +49,29 @@ const AnnouncementModal = ({ isOpen, onClose, onAddAnnouncement }) => {
                 id="title"
                 value={title}
                 onChange={(e) => setTitle(e.target.value)}
-                className="w-full p-2 rounded-lg bg-gray-700 text-gray-200"
+                className="w-full p-2 rounded-lg bg-white text-gray-200"
                 placeholder="Enter title"
                 required
               />
             </div>
             <div className="mb-4">
-              <label className="block text-gray-200 text-sm font-bold mb-2" htmlFor="body">
+              <label className="block text-black text-sm font-bold mb-2" htmlFor="body">
                 Body
               </label>
               <textarea
                 id="body"
                 value={body}
                 onChange={(e) => setBody(e.target.value)}
-                className="w-full p-2 rounded-lg bg-gray-700 text-gray-200"
+                className="w-full p-2 rounded-lg bg-white text-gray-200"
                 rows="4"
                 placeholder="Enter announcement"
+                style={{ width: '430px', height: '150px', border: '0.5px solid #000' }}
                 required
               />
             </div>
             <button
               type="submit"
-              className="w-full py-2 px-4 bg-yellow-500 font-press-start text-gray-800 font-bold rounded-lg hover:bg-yellow-400"
+              className="w-full py-2 px-4 bg-gray-700 font-press-start text-white font-bold rounded-lg hover:bg-yellow-400"
             >
               Add Announcement
             </button>

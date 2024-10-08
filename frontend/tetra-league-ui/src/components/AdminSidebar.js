ad411d71ecb34a278efee1dbf5126d28034b31aa
geraldloi <gerald.loi.2023@scis.smu.edu.sg>
2024-10-08 21:12:27 +0800

    Fixed AdminSidebar

--- frontend/tetra-league-ui/src/components/AdminSidebar.js
+++ frontend/tetra-league-ui/src/components/AdminSidebar.js
@@ -8,29 +8,8 @@ const AdminSidebar = ({ name, imageUrl }) => {
   return (
     <nav className="bg-white bg-opacity-80 pt-4 pb-4 fixed top-12 left-0 h-full w-60 z-50 shadow-md flex flex-col">
       <div className="flex flex-col">
+
         <div className="flex font-hevetica-neue font-medium text-customGray pb-4 pl-4 pt-4">
-          Ongoing Tournaments
-        </div>
-
-        <ul className="flex flex-col font-light text-customGray gap-8 text-l pb-8">
-          {/*link to Tournament Page*/}
-          <li>
-            <Link
-              to="/manage-tournaments"
-              className="flex items-center bg-customRed bg-opacity-30 pt-4 pb-4 pl-4 pr-4">
-                <div className="w-12 h-12 mr-4 rounded-full overflow-hidden flex-shrink-0">
-                  <img
-                    src={imageUrl} // Update with your image path
-                    alt="Tournament Icon"
-                    className="w-full h-full object-cover justify-center items-center" // Adjust size as needed
-                  />
-                </div>
-                {name}
-            </Link>
-          </li>
-        </ul>
-
-        <div className="flex font-hevetica-neue font-medium text-customGray pb-4 pl-4">
           Admin Tools
         </div>
 
@@ -47,7 +26,7 @@ const AdminSidebar = ({ name, imageUrl }) => {
           <li>
             <Link
               to="/create-tournament"
-              className={`flex items-center space-x-4 pt-4 pb-4 pl-4 font-light font-hevetica-neue text-customGray ${location.pathname === '/manage-participants' ? 'text-white font-medium bg-customGray bg-opacity-80' : 'hover:text-yellow-500'}`}
+              className={`flex items-center space-x-4 pt-4 pb-4 pl-4 font-light font-hevetica-neue text-customGray ${location.pathname === '/create-tournament' ? 'text-white font-medium bg-customGray bg-opacity-80' : 'hover:text-yellow-500'}`}
             >
               <LuPlusCircle className="text-3xl" />
               <span>Tournament Creator</span>
@@ -56,7 +35,7 @@ const AdminSidebar = ({ name, imageUrl }) => {
           <li>
             <Link
               to="/manage-tournaments"
-              className={`flex items-center space-x-4 pt-4 pb-4 pl-4 font-light font-hevetica-neue text-customGray ${location.pathname === '/manage-schedules' ? 'text-white font-medium bg-customGray bg-opacity-80' : 'hover:text-yellow-500'}`}
+              className={`flex items-center space-x-4 pt-4 pb-4 pl-4 font-light font-hevetica-neue text-customGray ${location.pathname === '/manage-tournaments' ? 'text-white font-medium bg-customGray bg-opacity-80' : 'hover:text-yellow-500'}`}
             >
               <LuCalendar className="text-3xl" />
               <span>My Tournaments</span>
@@ -65,7 +44,7 @@ const AdminSidebar = ({ name, imageUrl }) => {
           <li>
             <Link
               to="/admin-past-tournaments"
-              className={`flex items-center space-x-4 pt-4 pb-4 pl-4 font-light font-hevetica-neue text-customGray ${location.pathname === '/manage-results' ? 'text-white font-medium bg-customGray bg-opacity-80' : 'hover:text-yellow-500'}`}
+              className={`flex items-center space-x-4 pt-4 pb-4 pl-4 font-light font-hevetica-neue text-customGray ${location.pathname === '/admin-past-tournaments' ? 'text-white font-medium bg-customGray bg-opacity-80' : 'hover:text-yellow-500'}`}
             >
               <LuArchive className="text-3xl" />
               <span>Past Tournaments</span>

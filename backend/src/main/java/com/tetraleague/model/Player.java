f84593d67821edcb95743b1da4552021030fe04c
ftdurga <durgad.c.2023@scis.smu.edu.sg>
2024-10-09 19:42:18 +0800

    Removed update methods from player, added 2 attributes for glicko

--- backend/src/main/java/com/tetraleague/model/Player.java
+++ backend/src/main/java/com/tetraleague/model/Player.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.annotation.TypeAlias;
-// import org.springframework.data.mongodb.core.aggregation.DocumentOperators.Rank;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
@@ -16,31 +15,39 @@ import lombok.Setter;
 @Document("users")
 @TypeAlias("player")
 public class Player extends User {
-    
+
+
+    // set default values for rating, deviation and volatility
     @Field
-    private int eloRating;
+    private double eloRating = 1000.00; 
+
+    @Field
+    private double ratingDeviation = 200.00;
+
+    @Field
+    private double volatility = 0.06;
 
     @Field
     private List<Tournament> tournaments = new ArrayList<>();
 
-    // set to default values 0 inside constructor
+    // set to default values as 0 for game stats 0 
     @Field
     private int gamesWon = 0;
 
     @Field
     private int gamesLost = 0;
 
-    @Field
     // All players start with Unranked
+    @Field
     private Rank rank = Rank.UNRANKED;
 
-    //do not need game stats in constructor as they have default values
-    public Player(String username, String email, String password, int eloRating) {
+
+    public Player(String username, String email, String password, double eloRating) {
         super(username, email, password);
         this.eloRating = eloRating;
     }
 
-    //Getter and Setter for tournaments
+    // Getter and Setter for tournaments
     public void addTournament(Tournament tournament) {
         tournaments.add(tournament);
     }
@@ -49,45 +56,31 @@ public class Player extends User {
         tournaments.remove(tournament);
     }
 
-    //Getter and setter for elo-rating (for update)
-    public int getEloRating() {
-        return eloRating;
-    }
-
-    public void setEloRating(int newEloRating) {
-        eloRating = newEloRating; 
-    }
-
-    // Update games won and games lost
-    //if player wins - value true
-    public void updateGameResults(boolean won) {
-        if (won) {
-            gamesWon++;
-        } else {
-            gamesLost++;
+    //Modified getter method for getWinRate
+    public double getWinRate() {
+        if ((gamesLost + gamesWon) == 0) {
+            return 0.0;
         }
+        return gamesWon / (gamesLost + gamesWon) * 100.0;
     }
 
-    // Update the rank - takes in elorating
-    public void updateRank(int eloRating) {
-        if (eloRating >= 4500) {
-            rank = Rank.PLATINUM;
-        } else if (eloRating >= 3000) {
-            rank =  Rank.GOLD;
-        } else if (eloRating >= 2000) {
-            rank =  Rank.SILVER;
-        } else if ((gamesWon + gamesLost) < 10) {
-            rank =  Rank.UNRANKED;
-        } else {
-            rank =  Rank.BRONZE;
-        }
+    // Method to format a value to 2 decimal places
+    public double formatToTwoDecimalPlaces(double value) {
+        return Double.parseDouble(String.format("%.2f", value));
     }
 
-    // //Calculate winRate for playerDTO to display
-    // public double getWinRate() {
-    //     if((gamesLost + gamesWon) == 0) {
-    //         return 0.0;
-    //     }
-    //     return gamesWon / (gamesLost + gamesWon) * 100.0;
-    // }
+    // Modified Setter for eloRating to 2dp
+    public void setEloRating(double eloRating) {
+        this.eloRating = formatToTwoDecimalPlaces(eloRating);
+    }
+
+    // Modified Setter for ratingDeviation to 2dp
+    public void setRatingDeviation(double ratingDeviation) {
+        this.ratingDeviation = formatToTwoDecimalPlaces(ratingDeviation);
+    }
+
+    // Modified Setter for volatility to 2dp
+    public void setVolatility(double volatility) {
+        this.volatility = formatToTwoDecimalPlaces(volatility);
+    }
 }

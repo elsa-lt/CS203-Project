55065362e8bf55a25c30fc41081851e1519b73da
Varidhi <161668398+Varidhi123456@users.noreply.github.com>
2024-10-14 00:37:31 +0800

    leaderboard page

--- frontend/tetra-league-ui/src/pages/player/LeaderboardPage.js
+++ frontend/tetra-league-ui/src/pages/player/LeaderboardPage.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import Navbar from '../../components/UserNavbar';
+import LeaderBoardSubtabs from '../../components/LeaderBoardSubtabs';
 
 const LeaderboardPage = () => {
   return (
     <main
-      className="flex min-h-screen items-center justify-center bg-cover bg-center p-6"
+      className="flex min-h-screen bg-cover bg-center p-10"
       style={{ backgroundImage: `url('/Background/White Background.png')` }}
     >
+      <div className="fixed">
         <Navbar />
-        <div className="w-1/2 p-8 relative">
-        <div className="relative -top-40 -left-10 text-center">
-          <img src="/Headers/Leaderboard Header.png" alt="Profile" className="w-72 md:w-6/12 -ml-80"  style={{ width: '550px' }} />
-          </div>
-          </div>
+      </div>
+
+      <div className="flex flex-col min-h-screen w-full mt-14 ml-5 mr-5">
+        <div className="flex flex-grow-0 w-30 h-10">
+          <img 
+            src="/Headers/LeaderBoard Header.png"
+            alt="Leaderboard Header"
+            className="w-30 h-10"
+          />
+        </div>
+
+        <div className="mt-6 mb-6">
+          <hr className="w-full border-customGray border-opacity-30"/>
+        </div>
+
+        <LeaderBoardSubtabs />
+      </div>
     </main>
   );
 };
-
 export default LeaderboardPage;

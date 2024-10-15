530fdf5dfc968dd527df7b71ab9d16de1b8f4bbf
geraldloi <gerald.loi.2023@scis.smu.edu.sg>
2024-10-16 06:59:43 +0800

    Added getByIdFunction for Match

--- backend/src/main/java/com/tetraleague/controller/MatchController.java
+++ backend/src/main/java/com/tetraleague/controller/MatchController.java
@@ -1,6 +1,12 @@
 package com.tetraleague.controller;
 
+import com.tetraleague.model.Match;
 import com.tetraleague.service.MatchService;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,6 +18,17 @@ public class MatchController {
     public MatchController(MatchService matchService) {
         this.matchService = matchService;
     }
+
+    @GetMapping("/{matchId}")
+    public ResponseEntity<Match> getMatchById(@PathVariable String tournamentId, @PathVariable String matchId) {
+        try {
+            Match match = matchService.getMatchById(matchId);
+            return ResponseEntity.ok(match);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
 
     @PutMapping("/{matchId}/result")
     public String completeMatch(
